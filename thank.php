689d8616be89301c850550b9a6b849d7e1375462
raviraj.kushvaha <raviraj.kushvaha@healthelife.in>
2019-08-20 10:05:50 +0530

    Adding website files

---
+++ thank.php
@@ -0,0 +1,332 @@
+<?php
+error_reporting(0);
+		extract($_POST);
+		if(isset($submit)){
+			$to = "info@leisurepromotion.com";
+			$subject = "Enquiry";
+			$txt ="Name:".$name."Email:".$email."Message:".$message."Phone:".$mobile;
+			$headers = "From:'.$email.' " . "\r\n" .
+			mail($to,$subject,$txt,$headers);
+			echo("<script>alert('Successfully Sent');</script>");
+}
+?>
+
+<!DOCTYPE html>
+<html class="" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- GOOGLE FONT -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700,400italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,600,700' rel='stylesheet' type='text/css'>
+
+    <!-- CSS LIBRARY -->
+    <!-- Bootstrap -->
+    <link type="text/css" href="css/lib/bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="css/lib/jquery.bootstrap-touchspin.min.css" rel="stylesheet">
+
+    <!-- Font Icon -->
+    <link type="text/css" href="css/lib/font-awesome.min.css" rel="stylesheet">
+    <link type="text/css" href="css/lib/cortana.css" rel="stylesheet">
+
+    <!-- Revolution Slider -->
+    <link type="text/css" href="rs-plugin/css/settings.css" rel="stylesheet">
+    <!-- Owl Carousel -->
+    <link type="text/css" href="css/lib/owl.carousel.css" rel="stylesheet">
+
+    <!-- Select into div -->
+    <link type="text/css" href="css/lib/select2.min.css" rel="stylesheet">
+
+    <!-- Magnific Popup -->
+    <link type="text/css" href="css/lib/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link type="text/css" href="css/style.css" rel="stylesheet">
+
+   
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <title>Addmarc</title>
+	<style>
+		.bg-white p {text-align:justify;}
+	</style>
+</head>
+<body>
+
+<!-- HEADER 6 -->
+<header class="header head6">
+   
+    <div class="top-header top-header-3" style="height:40px;">
+       
+    </div><!-- /top-header -->
+
+    <div class="main-header main-header-6">
+        <div class="container-fluid" style="padding:0 60px;">
+            <div class="col-md-4">
+            <div class="logo">
+                <a href="index.html"><img src="img/logo.png" alt="logo"></a>
+            </div><!-- /#logo -->
+            <!-- END / LOGO -->
+			</div>
+			<div class="col-md-4">
+				
+			</div>
+            <!-- INFO CONTACT -->
+			<div class="col-md-4">
+            <div class="info-contact">
+                <h3 style="color:#46A5AE; text-align:right; line-height:30px;">Call Us : 011-26227222 / 333<br>(+91) 981 851 1337</h3>
+				<ul class="my-social">
+					<li><a href="https://www.linkedin.com/in/addmarc-a-unit-of-leisure-promotion-pvt-ltd-37416015b/" target="_blank"><img src="img/li.png" alt="instagram"></a></li>	
+					<li><a href="https://plus.google.com/114557283295273837544"><img src="img/gp.png" alt="instagram"></a></li>
+					<li><a href="https://www.instagram.com/leisurepromotion/?hl=en"><img src="img/in.png" alt="instagram"></a></li>				
+					<li><a href="https://twitter.com/LeisurePromotio"><img src="img/tw.png" alt="instagram"></a></li>
+					<li><a href="https://www.facebook.com/ADDMARCADVERTISING?ref=bookmarks"><img src="img/fb.png" alt="instagram"></a></li>
+				</ul>
+            </div><!-- /info-contact -->
+			</div>
+            <!-- END / INFO CONTACT -->
+			
+        </div><!-- /container -->
+    </div>
+
+    <div class="navBar6">
+         <div class="container-fluid" style="padding:0 60px;">
+            <!-- NAVIGATION -->
+            <nav class="navigation nav6" data-menu-type="991">
+                <ul class="nav text-uppercase">
+                    <li>
+                        <a href="index.html">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="about.html">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="services.html">
+                            Our Services
+                        </a>
+                    </li>
+                    <li>
+						<a href="marketing.html">
+							Marketing
+						</a>
+					</li>
+                    <li>
+                        <a href="clients.html">
+                            Our Clients
+                        </a>
+                    </li>
+                    <li>
+                        <a href="presence.html">
+                            Our Presence
+                        </a>
+                    </li>
+                    <li>
+						<a href="contact.html">
+							Contact us
+						</a>
+					</li>
+                </ul><!-- /nav -->
+            </nav>
+            <!-- END / NAVIGATION -->
+
+            <!-- MOBILE MENU -->
+            <div class="mobile-nav-menu" data-menu-number="6">
+                <span class="icon">&nbsp;</span>
+            </div><!-- /mobile-nav-menu -->
+            <!-- /END MOBILE MENU -->
+
+        </div><!-- /container -->
+    </div><!-- /navBar6 -->
+</header><!-- /header -->
+<!-- END / HEADER 6 -->
+
+<div id="page-wrap">
+    
+    
+    <section class="section bg-white pt-40" style="border-top:8px solid #46A5AE; padding-bottom:0px;">
+        <div class="container-fluid" style="padding:0 60px;">
+            <div class="row">
+				<h3 style="text-align:center; margin:70px 0;">Thank You</h3>
+			</div>
+        </div><!-- /container -->
+    </section>
+    <!-- END / ABOUT -->
+
+     <!-- COUNTER 3 -->
+    <section class="section bg-parallax pb-100" data-background="img/image1.jpg" style="margin-bottom:70px;">
+        <div class="bg-overlay-black bg-overlay-7"></div>
+        <div class="container-fluid" style="padding:0 60px;">
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-12" style="padding:0 40px;">
+                    <div class="counter">
+                        <p class="count-number">700</p>
+                        <h3>Clients</h3>
+                    </div><!-- /counter -->
+                </div><!-- /col -->
+                <div class="col-md-3 col-sm-6 col-xs-12" style="padding:0 40px;">
+                    <div class="counter">
+                        <p class="count-number">100000</p>
+                        <h3>Customers</h3>
+                    </div><!-- /counter -->
+                </div><!-- /col -->
+                <div class="col-md-3 col-sm-6 col-xs-12" style="padding:0 40px;">
+                    <div class="counter">
+                        <p class="count-number">20</p>
+                        <h3>States</h3>
+                    </div><!-- /counter -->
+                </div><!-- /col -->
+                <div class="col-md-3 col-sm-6 col-xs-12" style="padding:0 40px;">
+                    <div class="counter">
+                        <p class="count-number">250</p>
+                        <h3>Team Members</h3>
+                    </div><!-- /counter -->
+                </div><!-- /col -->
+            </div><!-- /row -->
+        </div><!-- /container -->
+        <div class="stat-trigger"></div>
+    </section>
+    <!-- END / COUNTER 3 -->
+   
+    <!-- FOOTER -->
+    <footer id="footer" class="footer bg-dark">
+        <div class="foot-widget">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <aside class="widget widget_infomation">
+                            <h3 class="widget-title h4"><strong>Website Links</strong></h3>
+                            <ul class="list-style">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about.html">About Us</a></li>
+                                <li><a href="services.html">Our Services</a></li>
+                                <li><a href="marketing.html">Marketing</a></li>
+                            </ul>
+                        </aside><!-- /widget_infomation -->
+                    </div><!-- /col -->
+					<div class="col-md-4 col-sm-4 col-xs-12">
+                        <aside class="widget widget_infomation">
+                            <h3 class="widget-title h4"><strong>Quick Links</strong></h3>
+                            <ul class="list-style">
+                                <li><a href="clients.html">Our Clients</a></li>
+                                <li><a href="presence.html">Our Presence</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </aside><!-- /widget_infomation -->
+                    </div><!-- /col -->
+					<div class="col-md-4 col-sm-4 col-xs-12">
+                        <aside class="widget widget_contact">
+                            <h3 class="widget-title h4"><strong>Contact Us</strong></h3>
+                            <ul>
+                                <li><i class="fa fa-map-marker"></i> 248, Ground Floor, Sant Nagar, East Of Kailash, <br>New Delhi - 110065, Delhi (India)</li>
+                                <li><i class="icon icon-phone"></i> 011-26227222 / 26227333, 9818511337</li>
+                                <li><i class="icon icon-paper-plane"></i>info@leisurepromotion.com / info@addmarc.co</li>
+                                
+                            </ul>
+                        </aside><!-- /widget_contact -->
+                    </div><!-- /col -->
+                </div><!-- /row -->
+				<div class="row">
+					<p style="color:#fff; text-align:center;">Copyright &copy; 2018</p>
+				</div>
+            </div><!-- /container -->
+        </div><!-- /foot-widget -->
+
+    </footer>
+    <!-- END / FOOTER -->
+</div><!-- /page-wrap -->
+
+<div id="abc">
+<!-- Popup Div Starts Here -->
+<div id="popupContact">
+<!-- Contact Us Form -->
+<form action="thank.php" id="form" method="post" name="form">
+<img id="close" src="img/3.png" onclick ="div_hide()">
+<h2>Reach Us</h2>
+<hr>
+<input id="name" name="name" placeholder="Name" type="text">
+<input id="email" name="email" placeholder="Email" type="text">
+<input id="mobile" name="mobile" placeholder="Contact No." type="text">
+<textarea id="msg" name="message" placeholder="Message"></textarea>
+<input type="submit" value="Send" name="submit" style="background: #4FA9B2; color: #fff; font-size: 18px;">
+</form>
+</div>
+<!-- Popup Div Ends Here -->
+</div>
+<!-- Display Popup Button -->
+<button class="my_button" id="popup" onclick="div_show()">Reach Us</button>
+
+<!-- JQUERY -->
+    <script type="text/javascript" src="js/lib/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script type="text/javascript" src="js/lib/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.bootstrap-touchspin.min.js"></script>
+
+    <!-- Revolution Slider -->
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <!-- Owl Carousel -->
+    <script type="text/javascript" src="js/lib/owl.carousel.min.js"></script>
+    <!-- Overflow Text -->
+    <script type="text/javascript" src="js/lib/overflow-text.js"></script>
+
+    <!-- Easing -->
+    <script type="text/javascript" src="js/lib/jquery.easing.min.js"></script>
+
+    <!-- Select to div -->
+    <script type="text/javascript" src="js/lib/select2.min.js"></script>
+
+    <!-- Parallax -->
+    <script type="text/javascript" src="js/lib/jquery.parallax-1.1.3.js"></script>
+
+    <!-- Waypoint -->
+    <script type="text/javascript" src="js/lib/waypoints.min.js"></script>
+
+    <!-- Count To -->
+    <script type="text/javascript" src="js/lib/jquery.countTo.js"></script>
+
+    <!-- Magnific Popup -->
+    <script type="text/javascript" src="js/lib/jquery.magnific-popup.min.js"></script>
+
+    <!-- Image Loaded -->
+    <script type="text/javascript" src="js/lib/imagesloaded.pkgd.min.js"></script>
+
+    <!-- Isotope -->
+    <script type="text/javascript" src="js/lib/isotope.pkgd.min.js"></script>
+
+    
+    <!-- Custom jQuery -->
+    <script type="text/javascript" src="js/scripts.js"></script>
+	
+    <script>
+	// Validating Empty Field
+function check_empty() {
+if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+alert("Fill All Fields !");
+} else {
+document.getElementById('form').submit();
+alert("Form Submitted Successfully...");
+}
+}
+//Function To Display Popup
+function div_show() {
+document.getElementById('abc').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+document.getElementById('abc').style.display = "none";
+}
+	</script>
+    
+</body>
+
+</html>
